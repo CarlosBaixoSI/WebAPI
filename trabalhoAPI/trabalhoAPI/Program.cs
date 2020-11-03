623d6f0ae6a835a979c3a7a6419a7817e481c32c
Carlos Baixo <73673227+CarlosBaixoSI@users.noreply.github.com>
2020-11-03 04:28:11 +0000

    Add files via upload

---
+++ trabalhoAPI/trabalhoAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalhoAPI
+{
+    //CLiente
+    class Program
+    {
+
+        enum FileType
+        {
+            TXT,
+            JPG,
+            PNG
+
+        }
+        class Pdu
+        {
+            public header h = new header();
+            public FileContent u = new FileContent();
+        }
+
+
+        class header
+        {
+            public FileType fileType;
+            public string filename;
+        }
+
+
+        class FileContent
+        {
+            public BinaryFormatter formatter;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Pdu p = new Pdu();
+            p.h.fileType = FileType.TXT;
+            FileStream fs = new FileStream(@"C: \Users\joao\Desktop\Trabalho CM\WebApplication1\User.txt", FileMode.Open);
+            p.u.formatter.Serialize
+
+        }
+
+    }
+}
